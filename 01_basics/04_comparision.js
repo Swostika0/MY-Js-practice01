92610fed97a385c017fcc99ad2c70680180ba2a7
swostika Bajagain <sostikabajgai8@gmail.com>
2025-06-25 12:11:45 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,16 @@
+//conversion of same kind of datatypes can be done like this easily
+// console.log(2>1);
+
+//conversion of different data types cant be done it doenot give desired output
+// eg- console.log("2">1);
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+
+//strict check ie === checks the data type as well and only compares if the datatype is same
+console.log("trying the strict check");
+console.log("2"===2);
