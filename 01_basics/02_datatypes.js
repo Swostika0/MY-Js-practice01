84eb9a77d850b0e95e6f85956eebbdb2f885a762
swostika Bajagain <sostikabajgai8@gmail.com>
2025-06-25 09:14:02 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3) we are using node js not browser so pop up wont arise usring alert
+/* focus on code readability*/
+let name = "swosti"
+let age = 22
+let isLoggedIn = false
+
+/*
+primitive data types
+number=2 to power 53
+bigint
+string=> ""
+boolean => true or false
+null=> standalone value chaina kei vaneko
+undefined=> var declare garera val nadeko
+symbol=>used for uniqueness
+*/
+
+//object
+console.log(typeof "swosti")
+console.log(typeof age)
+console.log(typeof null) //null ma object type dekhaudai cha which is not even true at all
+
+console.log(typeof undefined)
